ee3845d3c82d64aaa0b73ed55dbd8883c2b973ce
Jason <Jpihlk@yahoo.com>
2020-01-18 10:55:29 -0500

    new test

--- src/Components/App.test.js
+++ src/Components/App.test.js
@@ -7,3 +7,15 @@ it('renders correctly when there are no items', () => {
   const tree = renderer.create(<Items />).toJSON();
   expect(tree).toMatchSnapshot();
 });
+
+it('renders correctly when there is one item', () => {
+  const items = ['one'];
+  const tree = renderer.create(<Items items={items} />).toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+it('renders correctly when there are multiple items', () => {
+  const items = ['one', 'two', 'three'];
+  const tree = renderer.create(<Items items={items} />).toJSON();
+  expect(tree).toMatchSnapshot();
+});
